423ee1f8607997c95599b85ac2276452f683f8ce
unknown <rezguiaymen200@gmail.com>
2024-04-22 10:01:08 +0100

    dashbord

--- project.client/src/app/app-routing.module.ts
+++ project.client/src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ListProjetComponent } from './list-projet/list-projet.component';
 import { MembreComponent } from './membre/membre.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 const routes: Routes = [
   { path: '',
     component: SideBarComponent,
     children: [
+      {path:'dashbord',component:DashbordComponent},
       { path: '', component: ListProjetComponent },
       { path: 'list-projet', component: ListProjetComponent },
       { path: 'membre', component: MembreComponent },
